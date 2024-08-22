9afa2fba28c0fa5f3abfb094af7d5be3198604ce
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-08-22 04:56:44 -0300

    agregamos el color de fondo y el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import FormularioTarea from './components/FormularioTarea';
+import Footer from './components/Footer.jsx';
 
 function App() {
   
@@ -8,10 +9,11 @@ function App() {
   return (
     <>
     <main className='container my-5'>
-      <h1 className='text-center'>Bienvenidos</h1>
-      <h2 className='text-center'>ingresa tus tareas</h2>
+      <h1 className='text-center text-light'>Bienvenidos</h1>
+      <h2 className='text-center text-light'>ingresa tus tareas</h2>
       <FormularioTarea></FormularioTarea>
     </main>
+    <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="bg-black text-white">
+            &copy; todos los derechos reservados
+        </footer>
+    );
+};
+
+export default Footer;
